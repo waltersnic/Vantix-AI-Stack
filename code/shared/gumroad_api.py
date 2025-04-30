d9f3238876ba84533472a1bfe45611f089ed5ef8
Nicholas Walters <waltersnick4218@gmail.com>
2025-04-30 14:07:14 -0500

    Add mock Gumroad API tool for DropBot

--- code/shared/gumroad_api.py
+++ code/shared/gumroad_api.py
@@ -1,1 +1,15 @@
+# /code/shared/gumroad_api.py
 
+class GumroadAPI:
+    def __init__(self):
+        self.published = []
+
+    def publish(self, product_data):
+        # Fake publish logic for now
+        product = {
+            "title": product_data.get("title", "Untitled Product"),
+            "description": product_data.get("description", ""),
+            "price": product_data.get("price", 0)
+        }
+        self.published.append(product)
+        return f"[GUMROAD MOCK] Launched '{product['title']}' at ${product['price']}"
