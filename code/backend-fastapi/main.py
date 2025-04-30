2210f00f6eadf0b048fb6d3f64b52d891397fbb1
Nicholas Walters <waltersnick4218@gmail.com>
2025-04-30 14:26:00 -0500

    Create main.py

---
+++ code/backend-fastapi/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"status": "Vantix backend is live"}
