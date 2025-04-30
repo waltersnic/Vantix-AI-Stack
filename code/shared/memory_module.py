0a10456e29f871fcd124f7689902e276269afc72
Nicholas Walters <waltersnick4218@gmail.com>
2025-04-30 14:01:31 -0500

    Add agent memory module (short-term and long-term storage)

--- code/shared/memory_module.py
+++ code/shared/memory_module.py
@@ -1,1 +1,20 @@
+# /code/shared/memory_module.py
 
+class AgentMemory:
+    def __init__(self):
+        self.short_term = []
+        self.long_term = {}
+
+    def remember_short_term(self, item):
+        self.short_term.append(item)
+        if len(self.short_term) > 10:
+            self.short_term.pop(0)
+
+    def store_long_term(self, key, value):
+        self.long_term[key] = value
+
+    def retrieve_long_term(self, key):
+        return self.long_term.get(key, None)
+
+    def summarize_recent(self):
+        return "\n".join(self.short_term[-5:])
